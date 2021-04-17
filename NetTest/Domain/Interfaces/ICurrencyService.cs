804fd7d4780cac53e840ec5afffc67575c1b64ae
JM-Rosario <jmr4@cbord.com>
2021-04-17 17:50:24 -0400

    Interface to define currency services

---
+++ NetTest/Domain/Interfaces/ICurrencyService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+	public interface ICurrencyService
+	{
+		Task<Currency> GetDollarValueFromUrl(string url);
+	}
+}
