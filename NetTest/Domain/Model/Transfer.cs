55ad628b3cf8dbc112add96c5c0069400be1a25f
JM-Rosario <jmr4@cbord.com>
2021-04-17 17:49:36 -0400

    Model to represent Transfers

---
+++ NetTest/Domain/Model/Transfer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Model
+{
+	public class Transfer
+	{
+		public int ID { get; set; }
+		public int UserId { get; set; }
+		public decimal Exchange { get; set; }
+		public string CurrencyCode { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
