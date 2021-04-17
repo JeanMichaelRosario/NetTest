18c316db2c9a8427c017e7500ebcafae00eb28d3
JM-Rosario <jmr4@cbord.com>
2021-04-17 17:49:19 -0400

    Model to represent currencies

---
+++ NetTest/Domain/Model/Currency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Model
+{
+	public class Currency
+	{
+		public decimal Buy { get; set; }
+		public decimal Sell { get; set; }
+	}
+}
