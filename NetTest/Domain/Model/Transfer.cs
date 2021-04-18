cb88405dcdd2521a5b91ee62de5a557e3c17ff08
JM-Rosario <jmr4@cbord.com>
2021-04-17 20:26:52 -0400

    changing exchange for exchangeAmount on Transfer model

--- NetTest/Domain/Model/Transfer.cs
+++ NetTest/Domain/Model/Transfer.cs
@@ -8,7 +8,7 @@ namespace Domain.Model
 	{
 		public int ID { get; set; }
 		public int UserId { get; set; }
-		public decimal Exchange { get; set; }
+		public decimal ExchangeAmount { get; set; }
 		public string CurrencyCode { get; set; }
 		public DateTime Date { get; set; }
 	}
