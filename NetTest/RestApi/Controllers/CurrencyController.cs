aec27b46e73bae7965fd7bb195fcf581d3709003
Jean Michael Rosario <jmrosario94@gmail.com>
2021-04-19 01:53:53 -0400

    adding rest api

---
+++ NetTest/RestApi/Controllers/CurrencyController.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly ICurrencyService currencyService;
+        public CurrencyController(ICurrencyService dollar)
+        {
+            currencyService = dollar;
+        }
+
+        // GET api/<CurrencyController>/5
+        [HttpGet("{currencyCode}")]
+        public async Task<IActionResult> Get(string currencyCode)
+        {
+            IActionResult result;
+            try
+            {
+                var currency = await currencyService.GetCurrencyByCode(currencyCode);
+                result = Ok(currency);
+            }
+            catch (Exception ex)
+            {
+                result = NotFound(ex.Message);
+            }
+            return result;
+        }
+    }
+}
---
+++ NetTest/RestApi/Controllers/PurchaseController.cs
@@ -0,0 +1,39 @@
+﻿using Data.Common;
+using Domain.Interfaces;
+using Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseController : ControllerBase
+    {
+        private readonly ITransferService _validateTransfer;
+        public PurchaseController(ITransferService validateTransfer)
+        {
+            _validateTransfer = validateTransfer;
+        }
+
+        // POST api/<PurchaseController>
+        [HttpPost]
+        public IActionResult Post([FromBody]TransferHistory transfer)
+        {
+            try
+            {
+                var result = _validateTransfer.MakeTransaction(transfer);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
