a732ef4389948247b88e342fb99fdc6c93cbbb0c
Jean Michael Rosario <jmrosario94@gmail.com>
2021-04-19 00:55:38 -0400

    renaming currency tests

---
+++ NetTest/DomainTest/CurrencyTest.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Model;
+using Xunit;
+
+namespace DomainTest
+{
+    public class CurrencyTest
+    {
+        [Theory]
+        [InlineData(15, 18, 0.3, 4.5, 5.4, "brl")]
+        [InlineData(20, 24, 0.4, 8, 9.6, "brl")]
+        public void ExchangeTest(decimal buy, decimal sell, decimal exchangeRate, decimal expectedBuy, decimal expectedSell, string currencyCode)
+        {
+            var dollar = new Currency(buy, sell, "usd");
+            var real = new Currency(dollar, exchangeRate, currencyCode);
+
+            Assert.Equal(expectedBuy, real.Buy);
+            Assert.Equal(expectedSell, real.Sell);
+            Assert.Equal(currencyCode, real.CurrencyCode);
+        }
+
+        [Theory]
+        [InlineData(15, 18, "usd")]
+        [InlineData(20, 24, "usd")]
+        public void CreateCurrency(decimal buy, decimal sell, string currencyCode)
+        {
+            var dollar = new Currency(buy, sell, currencyCode);
+
+            Assert.Equal(buy, dollar.Buy);
+            Assert.Equal(sell, dollar.Sell);
+            Assert.Equal(currencyCode, dollar.CurrencyCode);
+        }
+    }
+}
