c6bb9c0bfb7c3acc1a69ceb3b1e8ec9197e392e2
Jean Michael Rosario <jmrosario94@gmail.com>
2021-04-19 00:56:26 -0400

    refactoring main currency class

--- NetTest/Domain/Model/Currency.cs
+++ NetTest/Domain/Model/Currency.cs
@@ -6,13 +6,22 @@ namespace Domain.Model
 {
 	public class Currency
 	{
-		public decimal Buy { get; set; }
-		public decimal Sell { get; set; }
+		public decimal Buy { get; private set; }
+		public decimal Sell { get; private set; }
+        public string CurrencyCode { get; private set; }
 
-		public Currency(decimal buy, decimal sell)
+        public Currency(decimal buy, decimal sell, string currencyCode)
 		{
 			Buy = buy;
 			Sell = sell;
+			CurrencyCode = currencyCode;
+		}
+
+		public Currency(Currency currency, decimal exchangeRate, string currencyCode)
+		{
+			Buy = currency.Buy * exchangeRate;
+			Sell = currency.Sell * exchangeRate;
+			CurrencyCode = currencyCode;
 		}
 	}
 }
